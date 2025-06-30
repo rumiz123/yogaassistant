3f3b086b92c9a338c2bc1ffc73fe8d2c950b70f6
Rumiz Nellasery <nrumiz@icloud.com>
2025-06-30 16:32:05 -0700

    Add user workout stats fields

--- app/src/main/java/com/rumiznellasery/yogahelper/data/UserStats.java
+++ app/src/main/java/com/rumiznellasery/yogahelper/data/UserStats.java
@@ -3,14 +3,20 @@ package com.rumiznellasery.yogahelper.data;
 public class UserStats {
     public String displayName;
     public int workouts;
+    public int totalWorkouts;
+    public int calories;
+    public int streak;
     public int score;
     public int level;
 
     public UserStats() {}
 
-    public UserStats(String displayName, int workouts, int score, int level) {
+    public UserStats(String displayName, int workouts, int totalWorkouts, int calories, int streak, int score, int level) {
         this.displayName = displayName;
         this.workouts = workouts;
+        this.totalWorkouts = totalWorkouts;
+        this.calories = calories;
+        this.streak = streak;
         this.score = score;
         this.level = level;
     }
