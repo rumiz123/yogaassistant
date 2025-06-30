159dae520f8c053105cdb0c47577692369a26a8d
rumiz123 <nrumiz@icloud.com>
2025-06-29 17:42:49 -0700

    added login firebase stuff

--- app/src/main/java/com/rumiznellasery/yogahelper/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/rumiznellasery/yogahelper/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("You Have No Notifications :)");
     }
 
     public LiveData<String> getText() {
