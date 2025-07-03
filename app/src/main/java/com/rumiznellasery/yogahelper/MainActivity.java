312824e2ae0a5ca9fbd65e0f7af46de0e42f6e4b
rumiz123 <nrumiz@icloud.com>
2025-07-02 21:31:22 -0700

    bar thing gone

--- app/src/main/java/com/rumiznellasery/yogahelper/MainActivity.java
+++ app/src/main/java/com/rumiznellasery/yogahelper/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Hide the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -33,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_leaderboard)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
